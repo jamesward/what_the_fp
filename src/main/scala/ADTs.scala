82143e6c234bc6cfbe15ffbda456e49a05e3fcd4
James Ward <james@jamesward.com>
2024-09-22 16:36:57 -0600

    more

--- src/main/scala/ADTs.scala
+++ src/main/scala/ADTs.scala
@@ -14,6 +14,10 @@ def main =
 
   println(a)
 
+  val justABoolean: Boolean | Boolean = true
+
+  val bORb: Either[Boolean, Boolean] = Right(true)
+
   // Enum Type
   enum IntOrString:
     case I(i: Int) extends IntOrString
@@ -33,26 +37,26 @@ def main =
     case IntOrString.S(s) => println(s"a string: $s")
 
   // Products
-  val p = (1, "a")
+  val intANDString: (Int, String) = (1, "a")
 
   case class Pair(i: Int, s: String)
   val p2 = Pair(1, "a")
 
 
   // Naming
-  val c: Boolean | Boolean = true
-  val d: Either[Boolean, Boolean] = Right(true)
 
   enum Trither(b: Boolean):
     case Left(b: Boolean) extends Trither(b)
     case Middle(b: Boolean) extends Trither(b)
     case Right(b: Boolean) extends Trither(b)
 
+  val bORbORb = Trither.Left(true)
+
   // 2 + 2 + 2
   // union / sum
   // OR
 
-  val bs: (Boolean, Boolean, Boolean) = (true, true, true)
+  val bANDbANDb: (Boolean, Boolean, Boolean) = (true, true, true)
 
   // 2 * 2 * 2
   // itersection / product
